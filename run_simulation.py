92be6a0c97b593e3cfc1a6e888f5bbf07e348a88
YannickBadoux <yannick@casema.nl>
2025-02-12 16:36:54 +0100

    started working on determining when a simulation needs to be stopped

--- run_simulation.py
+++ run_simulation.py
@@ -10,7 +10,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
 
-def run_simulation(bodies, plot=False, integrator='hermite'):
+def mean_square_distance(bodies):
+    'Returns the mean squared distance of the particle set'
+    square_distances = []
+    for i in range(len(bodies)):
+        for j in range(i+1, len(bodies)):
+            distance = (bodies[i].position - bodies[j].position).length()
+            square_distances.append(distance**2)
+    return np.mean(square_distances)
+
+def system_check(bodies):
+    '''
+    Check if the simulation needs to be stopped according to the procedure described in Appendix A 
+    of Hut & Bahcall 1983. 
+    Returns a boolean indicating if the simulation needs to be stopped and a code indicating the reason.
+    '''
+    
+    return stop, code
+
+def run_simulation(bodies, plot=False, integrator='hermite', save_path=None):
     converter = nbody_system.nbody_to_si(bodies.mass.sum(), bodies[1].position.length())
 
     if integrator == 'hermite':
@@ -27,17 +45,33 @@ def run_simulation(bodies, plot=False, integrator='hermite'):
 
     channel = gravity.particles.new_channel_to(bodies)
 
-    times = np.linspace(0, 10, 100) | units.yr
+    end_time = 2*(bodies[0].position - bodies[3].position).length() / bodies[3].velocity.length()
+    times = np.linspace(0, end_time.value_in(units.yr), 1000) | units.yr
 
     initial_energy = gravity.kinetic_energy + gravity.potential_energy
     energy_error = []
+    min_square_distance = min_square_distance(bodies)
 
-    for time in tqdm(times):
+    for time in times:
         gravity.evolve_model(time)
+
+        #check and save energy error
         error = np.abs((gravity.kinetic_energy + gravity.potential_energy - initial_energy) / initial_energy)
-        # print(error)
         energy_error.append(error)
+        
+        if error > 1e-2:
+            print(f"STOP! Energy error too high: {error}, Time: {int(time.value_in(units.yr))}")
+            break
+
         channel.copy()
+
+        min_square_distance = min([min_square_distance, mean_square_distance(bodies)])
+
+        #check if the simulation needs to be stopped
+        stop, code = system_check(bodies)
+        if stop:
+            print(f"STOP! Code {code}, Time: {int(time.value_in(units.yr))}")
+            break
 
         if plot:
             fig = plt.figure(figsize=(6,6))
@@ -45,9 +79,7 @@ def run_simulation(bodies, plot=False, integrator='hermite'):
             for body in bodies:
                 ax.scatter(body.x.value_in(units.AU), body.y.value_in(units.AU), body.z.value_in(units.AU),
                         label=body.name)
-                # ax.quiver(body.x.value_in(units.AU), body.y.value_in(units.AU), body.z.value_in(units.AU),
-                #             body.vx.value_in(units.kms)/10, body.vy.value_in(units.kms)/10, body.vz.value_in(units.kms)/10,
-                #             color=ax._get_lines.get_next_color())
+                
             ax.legend(loc='upper left')
             ax.set_xlabel('x [AU]')
             ax.set_ylabel('y [AU]')
@@ -55,10 +87,13 @@ def run_simulation(bodies, plot=False, integrator='hermite'):
 
             ax.set_xlim(-10,10)
             ax.set_ylim(-10,10)
-            ax.set_zlim(-1,5)
+            ax.set_zlim(-10,10)
             fig.savefig(f'../movie/simulation{int(time.value_in(units.day))}.png', dpi=200)
             plt.close()
 
     gravity.stop()
+
+    if save_path is not None:
+        write_set_to_file(bodies, save_path, 'amuse', overwrite_file=True)
 
     return bodies, energy_error
